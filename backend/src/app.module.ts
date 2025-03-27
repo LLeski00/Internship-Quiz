e824283d5df81dd5ec0c3675c858b2b80b0a4af6
Luka <llesko00@fesb.hr>
2025-03-27 16:35:51 +0100

    Added quiz and question resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizModule } from './quiz/quiz.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [],
+  imports: [QuizModule, QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
