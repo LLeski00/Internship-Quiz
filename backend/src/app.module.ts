fabdd22a8ac587a15faf87786f5d515b5963146d
Luka <llesko00@fesb.hr>
2025-03-28 17:31:38 +0100

    Add answer, category and score resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,12 @@ import { QuestionModule } from './question/question.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { AnswerModule } from './answer/answer.module';
+import { ScoreModule } from './score/score.module';
 
 @Module({
-  imports: [QuizModule, QuestionModule, PrismaModule, UserModule],
+  imports: [QuizModule, QuestionModule, PrismaModule, UserModule, CategoryModule, AnswerModule, ScoreModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
