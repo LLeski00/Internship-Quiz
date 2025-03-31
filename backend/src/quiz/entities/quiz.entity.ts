139102178a68e472d2c1e295a8cad8540c808bfa
Luka <llesko00@fesb.hr>
2025-03-31 17:32:57 +0200

    Add Api properties to quiz entity

--- backend/src/quiz/entities/quiz.entity.ts
+++ backend/src/quiz/entities/quiz.entity.ts
@@ -1,8 +1,14 @@
 import { Category, Quiz as PrismaQuiz } from '@prisma/client';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class Quiz {
+  @ApiProperty({ description: 'Unique identifier for the quiz' })
   id: string;
+
+  @ApiProperty({ description: 'Title of the quiz' })
   title: string;
+
+  @ApiPropertyOptional({ description: 'Category of the quiz' })
   category?: Category;
 
   constructor(id: string, title: string, category?: Category) {
