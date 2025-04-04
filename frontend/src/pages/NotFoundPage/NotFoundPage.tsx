f97375842f6a25189b99ea7b98ba8550ea4da726
Luka <llesko00@fesb.hr>
2025-04-04 18:04:34 +0200

    Add not found page

--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,21 @@
+import { routes } from "@/constants/routes";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage = () => {
-    return <></>;
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h2>The page doesn't exist</h2>
+            <Button
+                onClick={() => navigate(routes.HOME.path)}
+                variant="contained"
+            >
+                Go to home page
+            </Button>
+        </>
+    );
 };
 
 export default NotFoundPage;
